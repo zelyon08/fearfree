b2264d2d51a21cc357b34e03545fec9c6c5c54aa
Zeph NGADJUI <101606439+zelyon08@users.noreply.github.com>
2023-06-08 23:01:11 +0200

    -bd

--- anonymous/fearfree/crimes.sql
+++
@@ -1,9 +0,0 @@
-create table crimes
-(
-    id  int auto_increment
-        primary key,
-    nom varchar(100) not null,
-    constraint crimes_nom_uindex
-        unique (nom)
-);
-
--- anonymous/fearfree/roles.sql
+++
@@ -1,7 +0,0 @@
-create table roles
-(
-    role_id int auto_increment
-        primary key,
-    name    varchar(45) not null
-);
-
